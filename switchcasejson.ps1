27af316ee7cd16feb194ccf4ea5c04c69b8261b2
hostbyjake <git@hostedby.cloud>
2022-08-16 10:30:48 -0500

    updated switchcase to beta

--- switchcasejson.ps1
+++ switchcasejson.ps1
@@ -1,5 +1,33 @@
 $sharePointSite = 'https://vivifycompany.sharepoint.com/sites/SandreamSpecialties-CompanyFiles'
 $pathToExcel = 'C:\Users\jjones\Scripts\LogicAppVivify.xlsx'
+$intervalInMinutesToRun = '3'
+$sharedMailboxToParse = 'samples@vivifycompany.com'
+$subscription = '7be458ed-9d5e-4b79-bedb-b61e8510e45b'
+$regionName = "centralus"
+$resourceGroup = 'LogicFlow'
+$nameOfFlow = 'MyFlow'
+
+function Check-Command($cmdname) {
+    return [bool](Get-Command -Name $cmdname -ErrorAction SilentlyContinue)
+}
+
+if (Check-Command -cmdname 'az') {
+    Write-Host "Azure CLI is found..."
+}
+else {
+    Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'
+    az extension add --name logic
+    Write-Host "Please run 'az-login' and re-execute this script"
+    return
+}
+
+# $output = az account show -o json | ConvertFrom-Json
+# $subscriptionList = az account list -o json | ConvertFrom-Json 
+# $subscriptionName = $output.name
+# $subscriptionList | Format-Table name, id, tenantId, user -AutoSize
+# $accountID = $output.id
+
+
 
 #Install-module PSExcel -Force
 Import-module psexcel 
@@ -7,10 +35,9 @@ $people = new-object System.Collections.ArrayList
 
 foreach ($person in (Import-XLSX -Path $pathToExcel -RowStart 1)) {
     $people.add($person) | out-null 
-}
 
-$schema = "$" + "schema"
-$connections = "$" + "connections"
+
+
 
 $emailsToParse = $people.FROM | Select-Object -unique
 $locationToSend = $people.FOLDER | Select-Object -unique
@@ -23,44 +50,102 @@ for ($i = 1; $i -lt $emailsToParse.Count; $i++) {
     $senderToParse = $emailsToParse[$i]
     $EchoedStatement = 'If from ' + $senderToParse + ' going to ' + $folderPath
     Write-Output $EchoedStatement
-    
     $Case = @"
     "Case_$caseNumber": {
-    "actions": {
-        "For_each_$forEachNumber": {
-            "actions": {
-                "Create_file_$createFileNumber": {
-                    "inputs": {
-                        "body": "@items('For_each_$forEachNumber')",
-                        "host": {
-                            "connection": {
-                                "name": "@parameters('$connections')['sharepointonline']['connectionId']"
+        "actions": {
+            "For_each_$forEachNumber": {
+                "actions": {
+                    "Condition_$caseNumber": {
+                        "actions": {},
+                        "else": {
+                            "actions": {
+                                "Create_new_folder_$createFileNumber": {
+                                    "inputs": {
+                                        "body": {
+                                            "path": "$folderPath"
+                                        },
+                                        "host": {
+                                            "connection": {
+                                                "name": "@parameters('$connections')['sharepointonline']['connectionId']"
+                                            }
+                                        },
+                                        "method": "post",
+                                        "path": "/datasets/@{encodeURIComponent(encodeURIComponent('$sharePointSite'))}/tables/@{encodeURIComponent(encodeURIComponent('$subscription'))}/createnewfolder"
+                                    },
+                                    "runAfter": {},
+                                    "type": "ApiConnection"
+                                }
                             }
                         },
-                        "method": "post",
-                        "path": "/datasets/@{encodeURIComponent(encodeURIComponent('$sharePointSite'))}/files",
-                        "queries": {
-                            "folderPath": "/$folderPath",
-                            "name": "@triggerBody()?['subject']",
-                            "queryParametersSingleEncoded": true
-                        }
+                        "expression": {
+                            "and": [
+                                {
+                                    "equals": [
+                                        "@items('For_each_$forEachNumber')?['Name']",
+                                        "$folderPath"
+                                    ]
+                                }
+                            ]
+                        },
+                        "runAfter": {},
+                        "type": "If"
                     },
-                    "runAfter": {},
-                    "runtimeConfiguration": {
-                        "contentTransfer": {
-                            "transferMode": "Chunked"
-                        }
-                    },
-                    "type": "ApiConnection"
-                }
+                    "Create_file_$createFileNumber": {
+                        "inputs": {
+                            "body": "@items('For_each_$forEachNumber')",
+                            "host": {
+                                "connection": {
+                                    "name": "@parameters('$connections')['sharepointonline']['connectionId']"
+                                }
+                            },
+                            "method": "post",
+                            "path": "/datasets/@{encodeURIComponent(encodeURIComponent('$sharePointSite'))}/files",
+                            "queries": {
+                                "folderPath": "/Reporting/$FolderPath",
+                                "name": "@triggerBody()?['subject']",
+                                "queryParametersSingleEncoded": true
+                            }
+                        },
+                        "runAfter": {
+                            "Condition_$caseNumber": [
+                                "Succeeded"
+                            ]
+                        },
+                        "runtimeConfiguration": {
+                            "contentTransfer": {
+                                "transferMode": "Chunked"
+                            }
+                        },
+                        "type": "ApiConnection"
+                    }
+                },
+                "foreach": "@body('List_folder_$createFileNumber')",
+                "runAfter": {
+                    "List_folder_$createFileNumber": [
+                        "Succeeded"
+                    ]
+                },
+                "type": "Foreach"
             },
-            "foreach": "@triggerBody()?['attachments']",
-            "runAfter": {},
-            "type": "Foreach"
-        }
+            "List_folder_$createFileNumber": {
+                "inputs": {
+                    "host": {
+                        "connection": {
+                            "name": "@parameters('$connections')['sharepointonline']['connectionId']"
+                        }
+                    },
+                    "method": "get",
+                    "path": "/datasets/@{encodeURIComponent(encodeURIComponent('$sharePointSite'))}/folders/@{encodeURIComponent('%252fReporting')}"
+                },
+                "metadata": {
+                    "%252fReporting": "/Reporting"
+                },
+                "runAfter": {},
+                "type": "ApiConnection"
+            }
+        },
+        "case": "$senderToParse"
     },
-    "case": "$senderToParse"
-},
 "@
 $CaseList += ($Case)
 Write-Output $Case
@@ -69,11 +154,47 @@ $lastCaseEmailToParse = $emailsToParse[0]
 $lastCaseFolder = $locationToSend[0]
 $lastCaseNumber = $emailsToParse.Count
 $lastCaseIdentifier = $LastCaseNumber -1
+
 $lastCase = @"
 "Case_$lastCaseIdentifier": {
     "actions": {
         "For_each_$lastCaseNumber": {
             "actions": {
+                "Condition_$lastCaseIdentifier": {
+                    "actions": {},
+                    "else": {
+                        "actions": {
+                            "Create_new_folder_$lastCaseNumber": {
+                                "inputs": {
+                                    "body": {
+                                        "path": "$lastCaseFolder"
+                                    },
+                                    "host": {
+                                        "connection": {
+                                            "name": "@parameters('$connections')['sharepointonline']['connectionId']"
+                                        }
+                                    },
+                                    "method": "post",
+                                    "path": "/datasets/@{encodeURIComponent(encodeURIComponent('$sharePointSite'))}/tables/@{encodeURIComponent(encodeURIComponent('$subscription'))}/createnewfolder"
+                                },
+                                "runAfter": {},
+                                "type": "ApiConnection"
+                            }
+                        }
+                    },
+                    "expression": {
+                        "and": [
+                            {
+                                "equals": [
+                                    "@items('For_each_$lastCaseNumber')?['Name']",
+                                    "$lastCaseFolder"
+                                ]
+                            }
+                        ]
+                    },
+                    "runAfter": {},
+                    "type": "If"
+                },
                 "Create_file_$lastCaseNumber": {
                     "inputs": {
                         "body": "@items('For_each_$lastCaseNumber')",
@@ -85,12 +206,16 @@ $lastCase = @"
                         "method": "post",
                         "path": "/datasets/@{encodeURIComponent(encodeURIComponent('$sharePointSite'))}/files",
                         "queries": {
-                            "folderPath": "/$lastCaseFolder",
+                            "folderPath": "/Reporting/$lastCaseFolder",
                             "name": "@triggerBody()?['subject']",
                             "queryParametersSingleEncoded": true
                         }
                     },
-                    "runAfter": {},
+                    "runAfter": {
+                        "Condition_$lastCaseIdentifier": [
+                            "Succeeded"
+                        ]
+                    },
                     "runtimeConfiguration": {
                         "contentTransfer": {
                             "transferMode": "Chunked"
@@ -99,14 +224,38 @@ $lastCase = @"
                     "type": "ApiConnection"
                 }
             },
-            "foreach": "@triggerBody()?['attachments']",
-            "runAfter": {},
+            "foreach": "@body('List_folder_$lastCaseNumber')",
+            "runAfter": {
+                "List_folder_$lastCaseNumber": [
+                    "Succeeded"
+                ]
+            },
             "type": "Foreach"
+        },
+        "List_folder_$lastCaseNumber": {
+            "inputs": {
+                "host": {
+                    "connection": {
+                        "name": "@parameters('$connections')['sharepointonline']['connectionId']"
+                    }
+                },
+                "method": "get",
+                "path": "/datasets/@{encodeURIComponent(encodeURIComponent('$sharePointSite'))}/folders/@{encodeURIComponent('%252fReporting')}"
+            },
+            "metadata": {
+                "%252fReporting": "/Reporting"
+            },
+            "runAfter": {},
+            "type": "ApiConnection"
         }
     },
     "case": "$lastCaseEmailToParse"
 }
 "@
+}
+
+$schema = "$" + "schema"
+$connections = "$" + "connections"
 
 $workflowJson = @"
 {
@@ -199,16 +348,15 @@ $workflowJson = @"
                     "path": "/v2/SharedMailbox/Mail/OnNewEmail",
                     "queries": {
                         "folderId": "Inbox",
-                        "from": "jjones@bsgtech.com",
-                        "hasAttachments": false,
+                        "hasAttachments": true,
                         "importance": "Any",
-                        "includeAttachments": false,
-                        "mailboxAddress": "samples@vivifycompany.com"
+                        "includeAttachments": true,
+                        "mailboxAddress": "$sharedMailboxToParse"
                     }
                 },
                 "recurrence": {
                     "frequency": "Minute",
-                    "interval": 3
+                    "interval": $intervalInMinutesToRun
                 },
                 "splitOn": "@triggerBody()?['value']",
                 "type": "ApiConnection"
@@ -219,14 +367,14 @@ $workflowJson = @"
         "$connections": {
             "value": {
                 "office365": {
-                    "connectionId": "/subscriptions/7be458ed-9d5e-4b79-bedb-b61e8510e45b/resourceGroups/LogicFlow/providers/Microsoft.Web/connections/office365",
+                    "connectionId": "/subscriptions/$subscription/resourceGroups/LogicFlow/providers/Microsoft.Web/connections/office365",
                     "connectionName": "office365",
-                    "id": "/subscriptions/7be458ed-9d5e-4b79-bedb-b61e8510e45b/providers/Microsoft.Web/locations/centralus/managedApis/office365"
+                    "id": "/subscriptions/$subscription/providers/Microsoft.Web/locations/centralus/managedApis/office365"
                 },
                 "sharepointonline": {
-                    "connectionId": "/subscriptions/7be458ed-9d5e-4b79-bedb-b61e8510e45b/resourceGroups/LogicFlow/providers/Microsoft.Web/connections/sharepointonline",
+                    "connectionId": "/subscriptions/$subscription/resourceGroups/LogicFlow/providers/Microsoft.Web/connections/sharepointonline",
                     "connectionName": "sharepointonline",
-                    "id": "/subscriptions/7be458ed-9d5e-4b79-bedb-b61e8510e45b/providers/Microsoft.Web/locations/centralus/managedApis/sharepointonline"
+                    "id": "/subscriptions/$subscription/providers/Microsoft.Web/locations/centralus/managedApis/sharepointonline"
                 }
             }
         }
@@ -235,3 +383,5 @@ $workflowJson = @"
 "@
 
 Write-Output $workflowJson > .\workflowSwitch.json
+
+az logic workflow create --resource-group $resourceGroup --location $regionName --name $nameOfFlow --definition .\workflowSwitch.json
